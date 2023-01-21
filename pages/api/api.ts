5d08b842fa4944e702569457cace6f9f380b5890
John Bogil <johncbogil@gmail.com>
2023-01-21 12:45:53 -0500

    example connection to mdb working

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -1,18 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from "../../lib/mongodb";
 
 type Data = {
   response: any
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const rand = generateSmolURL()
-  const inputURL = req.body['key']['longURL']
-  res.status(200).json({ response: rand })
-}
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   const rand = generateSmolURL()
+//   const inputURL = req.body['key']['longURL']
+//   res.status(200).json({ response: rand })
+// }
 
 export function generateSmolURL() {
   const min = 1;
@@ -20,3 +21,21 @@ export function generateSmolURL() {
   const rand = min + Math.floor(Math.random() * (max - min));
   return rand
 }
+
+export default async (req: any, res: any) => {
+  try {
+      const client = await clientPromise;
+      const db = client.db("sample_mflix");
+
+      const movies = await db
+          .collection("movies")
+          .find({})
+          .sort({ metacritic: -1 })
+          .limit(10)
+          .toArray();
+
+      res.json(movies);
+  } catch (e) {
+      console.error(e);
+  }
+};
