eadbc7c2cc6e826d135dcb3d402707050ff8c94d
John Bogil <johncbogil@gmail.com>
2023-01-21 13:17:25 -0500

    write to db

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -6,14 +6,25 @@ type Data = {
   response: any
 }
 
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   const rand = generateSmolURL()
-//   const inputURL = req.body['key']['longURL']
-//   res.status(200).json({ response: rand })
-// }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const rand = generateSmolURL()
+  const long = req.body['key']['longURL']
+  // write to db wiht input
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("urls");
+    const obj = {short: rand, long: long};
+    const result = await db.collection('urls').insertOne(obj)   
+    // console.log(result)
+    res.status(200).json({ response: result })
+  } catch(error) {
+    console.log(error)
+  }
+}
 
 export function generateSmolURL() {
   const min = 1;
@@ -21,19 +32,3 @@ export function generateSmolURL() {
   const rand = min + Math.floor(Math.random() * (max - min));
   return rand
 }
-
-export default async (req: any, res: any) => {
-  try {
-      const client = await clientPromise;
-      const db = client.db("urls");
-
-      const movies = await db
-          .collection("urls")
-          .find({})
-          .toArray();
-
-      res.json(movies);
-  } catch (e) {
-      console.error(e);
-  }
-};
